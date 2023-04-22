16a0ad4f7b2ec6b713e91244b6d5b19f08a55188
sangyoon kim <sangyun0904@gmail.com>
2023-04-22 22:03:36 +0900

    Create app caffe

--- payhere_assignment/settings.py
+++ payhere_assignment/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "caffe.apps.CaffeConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
