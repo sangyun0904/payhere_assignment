2f84d0f370ca4a816561115779b76756adab55e1
sangyoon kim <sangyun0904@gmail.com>
2023-04-22 22:35:43 +0900

    Add model adminUser, product

--- caffe/models.py
+++ caffe/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class AdminUser(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+
+class Product(models.Model):
+    category = models.CharField(max_length=100)
+    price = models.IntegerField()
+    origin_price = models.IntegerField(),
+    name = models.CharField(max_length=100),
+    info = models.CharField(max_length=300),
+    barcode = models.CharField(max_length=100),
+    expire = models.DateTimeField(auto_now_add=True),
+    big_size = models.BooleanField(default=False)
